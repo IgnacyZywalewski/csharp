881f36275ac7bc7aa87d3d10950a423b038bd87c
ignacy <i.zywalewski@gmail.com>
2025-05-06 17:16:08 +0200

    Lista 1

--- lista_1/lista_1/Program.cs
+++ lista_1/lista_1/Program.cs
@@ -1,7 +1,68 @@
 ﻿class Program
 {
+    class Integer
+    {
+        public Integer(int new_counter, int new_value)
+        {
+            get_counter_limit = new_counter;
+            set_counter_limit = new_counter;
+            _value = new_value;
+        }
+
+        public int Value
+        {
+            get 
+            {
+                if (get_counter < get_counter_limit)
+                {
+                    get_counter++;
+                    return _value;
+                }
+                else
+                {
+                    Console.WriteLine("Przekroczono limit pobierania wartosc");
+                    return -1;
+                }
+                    
+            }
+
+            set 
+            {
+                if (set_counter < set_counter_limit)
+                {
+                    set_counter++;
+                    _value = value;
+                }
+                else
+                {
+                    Console.WriteLine("Przekroczono limit nadawania wartosc");
+                }
+            }
+        }
+
+        public void Ureguluj()
+        {
+            get_counter = 0;
+            set_counter = 0;
+        }
+
+        public void WypiszStan()
+        {
+            Console.WriteLine("Licznik pobierania wartości: {0}. Licznik nadawania wartości {1}\n", get_counter, set_counter);
+        }
+
+
+        private int _value = 0;
+        private int get_counter_limit = 0;
+        private int set_counter_limit = 0;
+        private int get_counter = 0;
+        private int set_counter = 0;
+    }
+
+
     static void Main(string[] args)
     {
+        //Zadanie 1
         int input = 0;
 
         while (true)
@@ -18,7 +79,6 @@
                 break;
             }
         }
-
 
         int width = 5;
         Console.Write("".PadLeft(width));
@@ -38,5 +98,18 @@
             }
             Console.WriteLine();
         }
+
+        Console.WriteLine();
+
+        //Zadanie 2
+        Integer integer = new Integer(5, 10);
+        integer.WypiszStan();
+        Console.WriteLine("Aktualna wartość: {0}", integer.Value);
+        integer.Value = 5;
+        Console.WriteLine("Zmiana wartości: {0}", integer.Value);
+        integer.WypiszStan();
+        integer.Ureguluj();
+        Console.WriteLine("Po resecie liczników:");
+        integer.WypiszStan();
     }
 }

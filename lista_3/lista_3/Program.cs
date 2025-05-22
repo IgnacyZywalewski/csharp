d9e58944194a2832eac037ecb95e946b57febea3
ignacy <i.zywalewski@gmail.com>
2025-05-22 18:53:05 +0200

    Lista 3

--- lista_3/lista_3/Program.cs
+++ lista_3/lista_3/Program.cs
@@ -1,78 +1,71 @@
-﻿class Obserwator
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Obserwator
 {
     public string Nazwa { get; }
     public double X { get; }
     public double Y { get; }
-    private record Sasiad(string Nazwa, double X, double Y, double Odleglosc);
-
-    private List<Sasiad> sasiedzi = new();
-
     public Obserwator(string nazwa, double x, double y)
     {
         Nazwa = nazwa;
         X = x;
         Y = y;
     }
-    public void NowyObserwator(object? sender, Obserwator nowy)
+    private record Sasiad(string Nazwa, double X, double Y, double Odleglosc);
+    private List<Sasiad> sasiedzi = new();
+    public void Nowy(object? sender, Obserwator nowy)
     {
         if (nowy == this) return;
 
-        double odl = ObliczOdleglosc(nowy);
+        double odl = Math.Sqrt(Math.Pow(X - nowy.X, 2) + Math.Pow(Y - nowy.Y, 2));
         sasiedzi.Add(new Sasiad(nowy.Nazwa, nowy.X, nowy.Y, odl));
 
         sasiedzi = sasiedzi.OrderBy(s => s.Odleglosc).Take(2).ToList();
     }
-
-    public void Wypisz()
+    public void Wypisz(object? sender, EventArgs e)
     {
         Console.WriteLine($"Jestem {Nazwa} – lista sąsiadów:");
-        foreach (Sasiad s in sasiedzi)
+        foreach (var s in sasiedzi)
         {
-            Console.WriteLine($" {s.Nazwa} x = {s.X:0.###} y = {s.Y:0.###} odl = {s.Odleglosc:0.###}");
+            Console.WriteLine($" {s.Nazwa} x={s.X:0.###} y={s.Y:0.###} odl={s.Odleglosc:0.###}");
         }
     }
-
-    private double ObliczOdleglosc(Obserwator inny)
-    {
-        return Math.Sqrt(Math.Pow(X - inny.X, 2) + Math.Pow(Y - inny.Y, 2));
-    }
 }
-
 
 class Tworca
 {
     private Random rand = new();
     private List<Obserwator> obserwatorzy = new();
 
-    public event EventHandler<Obserwator>? NowyObserwatorUtworzony;
+    public event EventHandler<Obserwator>? NowyObsZdarzenie;
+    public event EventHandler? WypiszObsZdarzenie;
 
     public void StworzObserwatora(string nazwa)
     {
         double x = rand.NextDouble();
         double y = rand.NextDouble();
+        var nowyObs = new Obserwator(nazwa, x, y);
 
-        var nowy = new Obserwator(nazwa, x, y);
-        
-        NowyObserwatorUtworzony += nowy.NowyObserwator;
-        NowyObserwatorUtworzony?.Invoke(this, nowy);
-        obserwatorzy.Add(nowy);
+        NowyObsZdarzenie += nowyObs.Nowy;
+        WypiszObsZdarzenie += nowyObs.Wypisz;
+
+        obserwatorzy.Add(nowyObs);
+
+        NowyObsZdarzenie?.Invoke(this, nowyObs);
     }
-
     public void WypiszWszystkich()
     {
-        foreach (Obserwator obs in obserwatorzy)
-        {
-            obs.Wypisz();
-        }
+        WypiszObsZdarzenie?.Invoke(this, EventArgs.Empty);
     }
 }
-
 
 class Program
 {
     static void Main()
     {
-        Tworca tworca = new Tworca();
+        Tworca tworca = new();
 
         for (int i = 0; i < 4; i++)
         {
